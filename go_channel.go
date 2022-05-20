ea3a35851a2353d23930c55905ab8dd98d95e7e0
cherhhhua <chornic@outlook.com>
2022-05-20 15:46:23 +0800

    primary learn

---
+++ go_channel.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		defer fmt.Println("子go程结束")
+
+		fmt.Println("子go程正在运行……")
+
+		c <- 666 //666发送到c
+	}()
+
+	num := <-c //从c中接收数据，并赋值给num
+
+	fmt.Println("num = ", num)
+	fmt.Println("main go程结束")
+}
---
+++ go_channel2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for {
+		//ok为true说明channel没有关闭，为false说明管道已经关闭
+		if data, ok := <-c; ok {
+			fmt.Println(data)
+		} else {
+			break
+		}
+	}
+
+	fmt.Println("Finished")
+}
